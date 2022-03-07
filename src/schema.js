bd7fb1ff301fbf7cdd2bf1231bbb87f354c546e4
Jinhoo Bong <jinhoobong@gmail.com>
2022-03-07 13:23:59 -0800

    Defining the Link Schema

---
+++ src/schema.js
@@ -0,0 +1,23 @@
+const { gql } = require("apollo-server");
+
+// Link schema should have an ID, the input Url, and the desired slug
+// queries should include grabbing a single link & grabbing all links
+// mutations: creating a link
+const typeDefs = gql`
+  type Link {
+    id: Int!
+    url: String!
+    slug: String!
+  }
+  # defining queries
+  type Query {
+    link(id: Int!): Link
+    allLinks: [Link!]!
+  }
+
+  type Mutation {
+    createLink(link: String!, slug: String!): Link!
+  }
+`;
+
+module.exports = typeDefs;
