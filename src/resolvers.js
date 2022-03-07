ec2b2b4497e41749da31660c5bc878ab661b383d
Jinhoo Bong <jinhoobong@gmail.com>
2022-03-07 15:20:15 -0800

    Added functionality to generate unique slugs

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,3 +1,21 @@
+var crypto = require("crypto");
+
+const usedSlugs = [];
+
+function generateUniqueSlug() {
+  var slug = crypto.randomBytes(2).toString("hex");
+  // if slug has already been used
+  if (usedSlugs.includes(slug)) {
+    // create new slug
+    slug = crypto.randomBytes(2).toString("hex");
+    usedSlugs.push(slug);
+    return slug;
+  } else {
+    // already unique
+    return slug;
+  }
+}
+
 const resolvers = {
   Query: {
     async link(root, { id }, { models }) {
@@ -9,6 +27,8 @@ const resolvers = {
   },
   Mutation: {
     async createLink(root, { url, slug }, { models }) {
+      // if slug is empty, we want to generate unique slug
+      slug = generateUniqueSlug();
       return models.Link.create({
         url,
         slug
