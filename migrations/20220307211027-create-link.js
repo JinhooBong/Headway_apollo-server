c248bcfa2b85c5e1441a11d98a22a552195923e7
Jinhoo Bong <jinhoobong@gmail.com>
2022-03-07 13:13:21 -0800

    updated Link model to not allow null url or slugs

--- migrations/20220307211027-create-link.js
+++ migrations/20220307211027-create-link.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Links', {
+    await queryInterface.createTable("Links", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,9 +9,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       url: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       slug: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
@@ -25,6 +27,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Links');
+    await queryInterface.dropTable("Links");
   }
 };
